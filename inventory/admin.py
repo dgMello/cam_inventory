16143f50e8fa17eb71a66c50f0e408cb2d4004ee
Doug Mello <dmello88@gmail.com>
2020-02-03 22:49:33 -0500

    created models that will be final!

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Server, Switch, Camera
+from .models import CameraManufacturer, Server, Switch, IpAddress, Camera
 
+admin.site.register(CameraManufacturer)
 admin.site.register(Server)
 admin.site.register(Switch)
+admin.site.register(IpAddress)
 admin.site.register(Camera)
--- inventory/models.py
+++ inventory/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Manufacturer(models.Model):
+class CameraManufacturer(models.Model):
     manu_name = models.CharField('manufacturer name', max_length=20, primary_key=True)
 
     def __str__(self):
@@ -25,20 +25,8 @@ class Switch(models.Model):
         return self.subnet
 
 
-class HostId(models.Model):
-    switch_subnet = models.ForeignKey(Switch, on_delete=models.CASCADE)
-    host_id = models.IntegerField()
-
-    def __str__(self):
-        return self.host_id
-
-    @property
-    def full_ip_address(self):
-        """Returns the fill IP address."""
-        return '%s .%s' % (self.switch_subnet, str(self.host_id))
-
-
 class IpAddress(models.Model):
+    switch_subnet = models.ForeignKey(Switch, on_delete=models.CASCADE)
     ip_address = models.GenericIPAddressField(protocol='IPv4')
 
     def __str__(self):
@@ -46,12 +34,11 @@ class IpAddress(models.Model):
 
 
 class Camera(models.Model):
-    switch = models.ForeignKey(Switch, on_delete=models.SET(None))
     server = models.ForeignKey(Server, on_delete=models.SET_NULL, null=True, blank=True)
-    make = models.ForeignKey(Manufacturer, on_delete=models.SET_NULL, null=True)
+    make = models.ForeignKey(CameraManufacturer, on_delete=models.SET_NULL, null=True)
     model = models.CharField(max_length=200)
     firmware = models.CharField(max_length=200)
-    ip_address = models.GenericIPAddressField(protocol='IPv4', unique=True)
+    ip_address = models.OneToOneField(IpAddress, on_delete=models.SET_NULL, null=True)
     mac_address = models.CharField(max_length=200)
     PLUGIN_CHOICES = [
         ('arecont', 'Arecont'),
@@ -74,4 +61,4 @@ class Camera(models.Model):
     ptz_caps = models.CharField(choices=PTZ_CHOICES, default='None', max_length=200)
 
     def __str__(self):
-        return self.ip_address
+        return self.ip_address.ip_address
